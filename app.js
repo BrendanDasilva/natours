a9d8e2b7c7cd8e29005da2f51378d3bcd4b057b0
Brendan Dasilva <brendan.dasilva@gmail.com>
2024-06-13 10:43:52 -0400

    Implemented CORS

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -32,6 +33,10 @@ app.set("views", path.join(__dirname, "views"));
 //////////////////////////////////////////
 ////////// GLOBAL MIDDLEWARES ////////////
 //////////////////////////////////////////
+// Implementing Cors - Access-Control-Allow-Origin *
+app.use(cors());
+
+app.options("*", cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
